5adfea70a33a857d071708ff3d6be34a620f1334
Vladislav Baranov <gangshit111z1@gmail.com>
2022-12-14 05:12:09 +0300

    Updated: settings

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -1,1 +1,3 @@
 from .base import *
+
+ADMIN_ENABLED = True
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
@@ -6,6 +7,7 @@ from cars.views import CarModelViewSet
 from callings.views import CallModelViewSet
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 
 schema_view = get_schema_view(
@@ -31,6 +33,9 @@ urlpatterns = [
     re_path(r'^swagger(.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('admin/', admin.site.urls),
     path('v1/', include(router.urls))
 ]
+
+
+if settings.ADMIN_ENABLED is True:
+    urlpatterns += [path('admin/', admin.site.urls),]
